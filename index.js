d2b11f71a5957ab2fdf7b401b2a618802c2229ed
maan013 <48920195+maan013@users.noreply.github.com>
2020-03-08 23:12:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const parent = document.getElementById('parent');
+
+let parentCount = 0;
+
+parent.addEventListener('click', function() {
+  parentCount++;
+  document.getElementById('parent-count').innerHTML = parentCount + "";
+  
+});
+
+const child = document.getElementById('child');
+
+let childCount = 0;``
+
+child.addEventListener('click', function(e) {
+  childCount++;
+
+  document.getElementById('child-count').innerHTML = childCount + "";
+  e.stopPropagation();
+  e.preventDefault();
+});
